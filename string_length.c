bd583deadf410c2a3c2efcb5972c0bc909bc488e
Manish <singhmanish040506@gmail.com>
2026-01-09 12:29:52 +0530

    Create string_length.c

---
+++ string_length.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    int i = 0 , length = 0;
+    printf("Enter the character\n");
+    scanf("%s" , str);
+
+    while (str[i] != '\0')
+    {
+        length++;
+        i++;
+    }
+    
+    printf("Length of string = %d" , length);
+return 0;
+}
